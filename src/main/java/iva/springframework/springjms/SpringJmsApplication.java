a8532f6ef7a1c8e24850792a7e2eaf77ce1c4401
Igor Varshavsky <igor.varshavsky@gmail.com>
2021-02-28 21:06:16 -0500

    Configured separate ActiveMQ and set configs

--- src/main/java/iva/springframework/springjms/SpringJmsApplication.java
+++ src/main/java/iva/springframework/springjms/SpringJmsApplication.java
@@ -1,19 +1,19 @@
 package iva.springframework.springjms;
 
-import org.apache.activemq.artemis.api.core.*;
-import org.apache.activemq.artemis.core.config.*;
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.security.Role;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
-import org.apache.activemq.artemis.core.server.JournalType;
-import org.apache.activemq.artemis.core.server.SecuritySettingPlugin;
-import org.apache.activemq.artemis.core.server.group.impl.GroupingHandlerConfiguration;
-import org.apache.activemq.artemis.core.server.metrics.ActiveMQMetricsPlugin;
-import org.apache.activemq.artemis.core.server.plugin.*;
-import org.apache.activemq.artemis.core.settings.impl.AddressSettings;
-import org.apache.activemq.artemis.core.settings.impl.ResourceLimitSettings;
-import org.apache.activemq.artemis.utils.critical.CriticalAnalyzerPolicy;
+//import org.apache.activemq.artemis.api.core.*;
+//import org.apache.activemq.artemis.core.config.*;
+//import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+//import org.apache.activemq.artemis.core.security.Role;
+//import org.apache.activemq.artemis.core.server.ActiveMQServer;
+//import org.apache.activemq.artemis.core.server.ActiveMQServers;
+//import org.apache.activemq.artemis.core.server.JournalType;
+//import org.apache.activemq.artemis.core.server.SecuritySettingPlugin;
+//import org.apache.activemq.artemis.core.server.group.impl.GroupingHandlerConfiguration;
+//import org.apache.activemq.artemis.core.server.metrics.ActiveMQMetricsPlugin;
+//import org.apache.activemq.artemis.core.server.plugin.*;
+//import org.apache.activemq.artemis.core.settings.impl.AddressSettings;
+//import org.apache.activemq.artemis.core.settings.impl.ResourceLimitSettings;
+//import org.apache.activemq.artemis.utils.critical.CriticalAnalyzerPolicy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -29,12 +29,12 @@ public class SpringJmsApplication {
 
 	public static void main(String[] args) throws Exception {
 
-		ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-				.setPersistenceEnabled(false)
-				.setJournalDirectory("target/data/journal")
-				.setSecurityEnabled(false)
-				.addAcceptorConfiguration("invm","vm://1"));
-		server.start();
+//		ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+//				.setPersistenceEnabled(false)
+//				.setJournalDirectory("target/data/journal")
+//				.setSecurityEnabled(false)
+//				.addAcceptorConfiguration("invm","vm://1"));
+//		server.start();
 		SpringApplication.run(SpringJmsApplication.class, args);
 	}
 
